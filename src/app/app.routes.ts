54140def210455420614fc113cf60c72d33feb8e
perturbatorandtonny <perturbatorandtonny@gmail.com>
2026-05-27 10:13:55 -0500

    feat(role): implement angular guard for differ views by rol

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,23 +5,26 @@ import { LoginComponent } from './pages/login/login';
 import { RegistroComponent } from './pages/registro/registro';
 import { InicioComponent } from './pages/inicio/inicio';
 
+import { roleGuard } from './guards/role-guard';
+
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'registro', component: RegistroComponent },
-  { path: 'inicio', component: InicioComponent },
-  { path: 'offers', component: OfferList },
-  { path: 'offers/new', component: OfferForm },
+  { path: 'registro', component: RegistroComponent, canActivate: [roleGuard(['usuario', 'moderador','admin'])] },
+  { path: 'inicio', component: InicioComponent,  },
+  { path: 'offers', component: OfferList, canActivate: [roleGuard(['usuario', 'admin'])] },
+  { path: 'offers/new', component: OfferForm, canActivate: [roleGuard(['usuario', 'admin'])] },
   {
     path: 'cards', children: [
-      { path: 'list', loadComponent: () => import('./pages/card-list/card-list').then(m => m.CardListComponent) },
-      { path: 'new', loadComponent: () => import('./pages/card-form/card-form').then(m => m.CardFormComponent) },
-      { path: 'edit/:id', loadComponent: () => import('./pages/card-form/card-form').then(m => m.CardFormComponent) }
+      { path: 'list', loadComponent: () => import('./pages/card-list/card-list').then(m => m.CardListComponent), canActivate: [roleGuard(["admin", "usuario"])] },
+      { path: 'new', loadComponent: () => import('./pages/card-form/card-form').then(m => m.CardFormComponent), canActivate: [roleGuard(["admin", "usuario"])] },
+      { path: 'edit/:id', loadComponent: () => import('./pages/card-form/card-form').then(m => m.CardFormComponent), canActivate: [roleGuard(["admin", "usuario"])] }
     ]
   },
   {
     path: 'moderator/cards',
-    loadComponent: () => import('./pages/card-review/card-review').then(m => m.CardReviewComponent)
+    loadComponent: () => import('./pages/card-review/card-review').then(m => m.CardReviewComponent),
+    canActivate: [roleGuard(['moderador', 'admin'])]
   },
   { path: '**', redirectTo: 'login' },
 ];
